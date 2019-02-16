c6c34f632b9ec00d08894e5dfe8adb42bce3f35e
Leandro Silva <lsdesenvolvimento@hotmail.com>
2019-02-16 19:33:03 -0200

    adicionando arquivo de configuração que define como o next exportara as paginas como estaticas

---
+++ next.config.js
@@ -0,0 +1,9 @@
+module.exports = {
+  exportPathMap: function() {
+    return {
+      '/': { page: '/' },
+      '/about': { page: '/about' },
+      '/p/481': { page: '/post', query: { id: 481 } }
+    }
+  }
+}
